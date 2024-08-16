9d5fae585cd0366a2a0646fd282b78da46b79282
DhivyaBalakumar <161305232+DhivyaBalakumar@users.noreply.github.com>
2024-08-16 13:57:47 +0530

    Add files via upload

---
+++ Operations.py
@@ -0,0 +1,22 @@
+# Mathematical Operations
+"""
+addition                        +
+subtraction                     -
+multiplication                  *
+division                        /
+modulus                         %
+exponential                     **
+floor division                  //
+"""
+
+op1=10
+op2=20
+print(op1)
+print(op2)
+print(op1+op2)
+print(op1-op2)
+print(op1*op2)
+print(op1/op2)
+print(op1%op2)
+print(op1**op2)
+print(op1//op2)
