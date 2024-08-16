fd41b959e641165763b5c49632f61110a8f4085a
DhivyaBalakumar <161305232+DhivyaBalakumar@users.noreply.github.com>
2024-08-16 12:23:18 +0530

    Add files via upload

---
+++ CommentsInPy.py
@@ -0,0 +1,21 @@
+print("Hello World")
+print("Python is my favourite programming language")
+
+# this is a single line comment
+
+'''This
+is 
+s
+multiline 
+comment 
+unless and until it is assigned to a variable'''
+
+#when we assign the same multiline comment to a variable ->
+a='''This
+is 
+s
+multiline 
+comment 
+unless and until it is assigned to a variable'''
+
+print(a)
